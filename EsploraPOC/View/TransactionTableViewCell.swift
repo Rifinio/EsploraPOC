4343bec4e79d7ff7fb546c4af0e2493b9afeb2ba
Adil BOUGAMZA <rifinio88@gmail.com>
2020-08-29 13:45:34 +0200

    Add BaseViewController + static transaction view controller

---
+++ EsploraPOC/View/TransactionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionTableViewCell.swift
+//  EsploraPOC
+//
+//  Created by Adil Bougamza on 29/08/2020.
+//  Copyright © 2020 Adil Bougamza. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
